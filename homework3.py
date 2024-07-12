ae080f20ebc24d3c2a06721d5c76d52297db51bb
Sergey1401 <fire1401@mail.ru>
2024-07-12 17:41:42 +0400

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,9 @@
+# Практическая работа по уроку "Динамическая типизация".
+name = 'Sergey'
+print('Name: ',name)
+age = 40
+print('Age: ',40)
+age = 41
+print('New Age: ',age)
+is_student = True
+print('Is Student: ',is_student)
